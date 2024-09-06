ec8f414a98d11ae6f44236e8a6a49ece4cef9391
dharmendra <dtechnicaldk@gmail.com>
2024-09-06 21:17:49 +0530

    outlined and filled rectangle working properly

--- script.js
+++ script.js
@@ -1,77 +1,74 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-const canvasHight = window.innerHeight - (window.innerHeight * 20) / 100;
-const canvasWidth = window.innerWidth - (window.innerWidth * 30) / 100;
-console.log(canvasWidth);
-canvas.height = canvasHight;
-canvas.width = canvasWidth;
+
+const tools = document.querySelectorAll(".button.tool");
+const filledShapeCheckBox = document.getElementById("filledShapes");
+
+// unversal veriable to be used in various place
 let color = "";
-let size = 2;
+let brushWidth = 5;
 let x1, y1, x2, y2;
 let isPointerdown = false;
+let activeTool = "brush";
+let snapshot;
 
-//different mouse condition functions;
-canvas.addEventListener("pointerdown", (e) => {
+// onload function to calibrate brush and canvas
+window.addEventListener("load", () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+});
+
+// fuction to activate a tool
+tools.forEach((tool) => {
+  tool.addEventListener("click", (e) => {
+    document.querySelector(".button.tool.active").classList.remove("active");
+    tool.classList.add("active");
+    activeTool = tool.id;
+    console.log(activeTool);
+  });
+});
+
+const drawCircle = (e) => {
+  console.log("wait i m drawing a circle");
+};
+const drawRectangle = (e) => {
+  console.log("drawing rect");
+  x2 = e.offsetX;
+  y2 = e.offsetY;
+
+  if (filledShapeCheckBox.checked) {
+    ctx.fillRect(x1, y1, x2 - x1, y2 - y1);
+  } else {
+    ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+  }
+};
+
+// function to set all neccsary attribute to begin drawing
+const beginDrawing = (e) => {
   isPointerdown = true;
-  // e.preventDefault();
   x1 = e.offsetX;
   y1 = e.offsetY;
-});
+  snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.lineWidth = brushWidth;
+};
+
+// function to decide what shape is to be drawn on canvas
+const drawing = (e) => {
+  if (!isPointerdown) return;
+  ctx.putImageData(snapshot, 0, 0);
+  if (activeTool === "brush") {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+  } else if (activeTool === "square") {
+    drawRectangle(e);
+  } else if (activeTool === "circle") {
+    drawCircle(e);
+  }
+};
+
+canvas.addEventListener("pointermove", drawing);
+canvas.addEventListener("pointerdown", beginDrawing);
 canvas.addEventListener("pointerup", () => {
   isPointerdown = false;
-  x1 = undefined;
-  y1 = undefined;
 });
-canvas.addEventListener("pointermove", (e) => {
-  // e.preventDefault();
-  if (isPointerdown) {
-    x2 = e.offsetX;
-    y2 = e.offsetY;
-
-    drawCircle(x2, y2);
-    drawLine(x1, y1, x2, y2);
-    x1 = x2;
-    y1 = y2;
-  }
-});
-canvas.addEventListener("pointercancel", () => {
-  console.log("pointerdown cancelled");
-});
-function drawCircle(x1, y1) {
-  ctx.moveTo(x1, y1);
-  ctx.beginPath();
-  ctx.arc(x1, y1, size, 0, 2 * Math.PI);
-  ctx.fillStyle = color;
-  ctx.fill();
-}
-function drawLine(x1, y1, x2, y2) {
-  ctx.beginPath();
-  ctx.moveTo(x1, y1);
-  ctx.lineTo(x2, y2);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = size * 2;
-  ctx.stroke();
-}
-
-function selectColor(e) {
-  color = e.target.value;
-}
-
-function increase() {
-  let strokeSize = document.getElementById("strokeSize");
-  size = size + 1;
-  strokeSize.innerHTML = size;
-}
-function decrease() {
-  let strokeSize = document.getElementById("strokeSize");
-  size = size - 1;
-  if (size < 1) {
-    size = 1;
-  }
-  strokeSize.innerHTML = size;
-}
-
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-}
